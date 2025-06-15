90b223d95f87ea9fbe4094e7632f9b2d05632c87
yashsharma9311 <yashppsharma26@gmail.com>
2025-06-15 23:47:02 +0530

    change

--- formvalidation/formvalidation.js
+++ formvalidation/formvalidation.js
@@ -9,6 +9,7 @@ let confirmpassword = document.querySelector("#ConfirmPassword").value
 let errname = document.querySelector("#errname")
 let errnumber = document.querySelector("#errnumber")
 let erremail = document.querySelector("#erremail")
+let errpassword = document.querySelector("#errpassword")
 let errconfirmpassword = document.querySelector("#errconfirmpassword")
 
 if(name==""){
@@ -21,13 +22,13 @@ else if(isNaN(number)){
     errnumber.innerHTML=" Please Enter Valid Number"
     return false
 
-}
+} 
 
 else if(number.length!=10){
     errnumber.innerHTML=" Please Enter Valid 10 Digit Number"
     return false
 
-}
+} 
 
  else if(!(email.includes('@') && email.includes('.com'))){
     erremail.innerHTML=" Please Enter Valid Email"
@@ -39,7 +40,7 @@ else if(number.length!=10){
     errpassword.innerHTML=" Please Enter Password"
     
     return false
-}
+} 
 
 else if(!(password.match(/[1234567890]/))
     && !(password.match(/[!@#$%^&*()_+]/))
